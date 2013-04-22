a27ff338dc2b6facb520f84ac0c759435f8e8d14
Stuart Herbert <stuart@stuartherbert.com>
2013-04-22 11:44:02 +0100

    New: initial unit test for StringExpects prose
    Former-commit-id: 7b70f4519bb5f6301b3fed0d655d91b1f8a2a54d [formerly 7b5791e426b94532190383dda730036293e146ed]
    Former-commit-id: e5bef780c219cb7f2dd035cce7d1b86ce8e84788

--- src/tests/unit-tests/php/DataSift/Prose/StringExpectsTest.php
+++ src/tests/unit-tests/php/DataSift/Prose/StringExpectsTest.php
@@ -44,24 +44,33 @@
 
 namespace DataSift\Storyplayer\Prose;
 
+use stdClass;
+use PHPUnit_Framework_TestCase;
+use DataSift\Storyplayer\StoryLib\Story;
 use DataSift\Storyplayer\PlayerLib\StoryTeller;
-use DataSift\Storyplayer\ProseLib\AssertionsBase;
-use DataSift\Stone\ComparisonLib\StringComparitor;
 
-/**
- * Assertions about the nature of, and contents of, strings
- *
- * @category   Libraries
- * @package    Storyplayer
- * @subpackage Prose
- * @author     Stuart Herbert <stuart.herbert@datasift.com>
- * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
- * @link       http://datasift.github.io/storyplayer
- */
-class StringExpects extends AssertionsBase
+class StringExpectsTest extends PHPUnit_Framework_TestCase
 {
-	public function __construct(StoryTeller $st, $params)
+	/**
+	 * @covers DataSift\Storyplayer\Prose\StringExpects::__construct
+	 */
+	public function testCanInstantiate()
 	{
-		parent::__construct($st, new StringComparitor($params[0]));
+	    // ----------------------------------------------------------------
+	    // setup your test
+
+	    $st = new StoryTeller(new Story());
+	    $expectedArray = array("hello, world!");
+
+	    // ----------------------------------------------------------------
+	    // perform the change
+
+	    $obj = new StringExpects($st, $expectedArray);
+
+	    // ----------------------------------------------------------------
+	    // test the results
+
+	    $this->assertTrue($obj instanceof StringExpects);
 	}
+
 }
