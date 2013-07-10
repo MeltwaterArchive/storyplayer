96c7e6bd3a447262a97c1e214e1e4cd67060a5d5
Stuart Herbert <stuart@stuartherbert.com>
2013-07-10 15:22:07 +0100

    New: -e switch for setting the environment to run against

---
+++ src/php/DataSift/Storyplayer/Cli/EnvironmentSwitch.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * Copyright (c) 2011-present Mediasift Ltd
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the names of the copyright holders nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  Libraries
+ * @package   Storyplayer/HostLib
+ * @author    Stuart Herbert <stuart.herbert@datasift.com>
+ * @copyright 2011-present Mediasift Ltd www.datasift.com
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link      http://datasift.github.io/storyplayer
+ */
+
+namespace DataSift\Storyplayer\Cli;
+
+use Phix_Project\CliEngine;
+use Phix_Project\CliEngine\CliEngineSwitch;
+use Phix_Project\CliEngine\CliResult;
+
+/**
+ * A nice generic '-v|--version' switch for your CLI tool
+ *
+ * @package     Phix_Project
+ * @subpackage  CliEngine
+ * @author      Stuart Herbert <stuart@stuartherbert.com>
+ * @copyright   2013-present Stuart Herbert. www.stuartherbert.com
+ * @license     http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link        http://www.phix-project.org
+ * @version     @@PACKAGE_VERSION@@
+ */
+class EnvironmentSwitch extends CliEngineSwitch
+{
+	public function __construct($envList)
+	{
+		// remember the list of available environments
+		$this->envList = $envList;
+	}
+
+	public function getDefinition()
+	{
+		// define our name, and our description
+		$def = $this->newDefinition('environment', 'set the environment to test against');
+
+		// what are the short switches?
+		$def->addShortSwitch('e');
+
+		// what are the long switches?
+		$def->addLongSwitch('environment');
+
+		// what is the required argument?
+		$def->setRequiredArg('<environment>', "the environment to test against");
+		$def->setArgValidator(new EnvironmentValidator($this->envList));
+
+		// all done
+		return $def;
+	}
+
+	public function process(CliEngine $engine, $invokes = 1, $params = array(), $isDefaultParam = false)
+	{
+		// write the version to the output
+		$engine->output->stdout->outputLine(
+			$engine->output->highlightStyle,
+			$engine->getAppVersion()
+		);
+
+		// tell the engine that it is done
+		return new CliResult(0);
+	}
+}
