8ff8ad7a9e32330522d1006b434e9fcae31b5724
Stuart Herbert <stuart@stuartherbert.com>
2013-07-16 16:21:35 +0100

    New: fromAws() module
    Former-commit-id: 7656eb85053f38a670371f309c9d8dc75464de3e

---
+++ src/php/DataSift/Storyplayer/Prose/AwsDetermine.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * Copyright (c) 2011-present Mediasift Ltd
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the names of the copyright holders nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  Libraries
+ * @package   Storyplayer/Prose
+ * @author    Stuart Herbert <stuart.herbert@datasift.com>
+ * @copyright 2011-present Mediasift Ltd www.datasift.com
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link      http://datasift.github.io/storyplayer
+ */
+
+namespace DataSift\Storyplayer\Prose;
+
+use DataSift\Storyplayer\ProseLib\E5xx_ActionFailed;
+use DataSift\Storyplayer\ProseLib\Prose;
+use DataSift\Storyplayer\PlayerLib\StoryTeller;
+
+use Aws\Common\Aws;
+
+/**
+ * generates AWS clients using the official SDK
+ *
+ * @category  Libraries
+ * @package   Storyplayer/Prose
+ * @author    Stuart Herbert <stuart.herbert@datasift.com>
+ * @copyright 2011-present Mediasift Ltd www.datasift.com
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link      http://datasift.github.io/storyplayer
+ */
+class AwsDetermine extends Prose
+{
+	public function getAwsClientFactory()
+	{
+		// shorthand
+		$st = $this->st;
+
+		// what are we doing?
+		$log = $st->startAction("create AWS client factory using official SDK");
+
+		// get the settings for Aws
+		$awsSettings = $st->fromEnvironment()->getAppSettings('aws');
+
+		// create the AWS client factory
+		$awsFactory = Aws::factory(array(
+			'key' => $awsSettings->key,
+			'secret' => $awsSettings->secret,
+			'region' => $awsSettings->region
+		));
+
+		// all done
+		$log->endAction();
+		return $awsFactory;
+	}
+
+	public function getEc2Client()
+	{
+		// shorthand
+		$st = $this->st;
+
+		// what are we doing?
+		$log = $st->startAction("create AWS client for EC2");
+
+		// get the Aws client factory
+		$awsFactory = $st->fromAws()->getAwsClientFactory();
+
+		// create the EC2 client
+		$ec2Client = $awsFactory->get('ec2');
+
+		// all done
+		$log->endAction();
+		return $ec2Client;
+	}
+}
