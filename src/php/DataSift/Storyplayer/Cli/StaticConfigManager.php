1002bba5af6e3c970333428b84e5e2878320732d
Michael Heap <m@michaelheap.com>
2013-09-10 16:28:17 +0100

    Fix: Allow a user to specify a personal config file without requiring a global config file

--- src/php/DataSift/Storyplayer/Cli/StaticConfigManager.php
+++ src/php/DataSift/Storyplayer/Cli/StaticConfigManager.php
@@ -45,6 +45,7 @@ namespace DataSift\Storyplayer\Cli;
 
 use DataSift\Stone\ConfigLib\JsonConfigLoader;
 use DataSift\Stone\ConfigLib\E5xx_ConfigFileNotFound;
+use DataSift\Stone\ConfigLib\LoadedConfig;
 
 /**
  * helper class for loading our static config files
@@ -61,11 +62,33 @@ class StaticConfigManager extends ConfigManagerBase
 	public function loadConfig($config)
 	{
 		// load the main config file
-		$newConfig = $this->configLoader->loadDefaultConfig();
+		try {
+			// We start with an empty config
+			$userConfig = new LoadedConfig;
+			// Then load our user config into it
+			$this->configLoader->loadUserConfig($userConfig);
 
-		// load any per-user overrides
-		$this->configLoader->loadUserConfig($newConfig);
+			// Then try and load our default configs
+			$newConfig = $this->configLoader->loadDefaultConfig();
 
+			// Merge our user config into the default config
+			$newConfig->mergeFrom($userConfig);
+
+		// We couldn't find our default config files
+		} catch (E5xx_ConfigFileNotFound $e){
+			// Did we load a user config though? Get the public class
+			// vars and count them. If there's > 0, we have some config
+			$availableKeys = get_object_vars($userConfig);
+			// Otherwise, just rethrow the exception
+			if (!count($availableKeys)){
+				throw $e;
+			}
+
+			// Move user config to $newConfig for merging into the
+			// current config
+			$newConfig = $userConfig;
+		}
+		
 		// merge the new config with the existing
 		$config->mergeFrom($newConfig);
 
