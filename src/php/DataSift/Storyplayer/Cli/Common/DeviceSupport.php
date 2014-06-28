9723a1e9b3e1b63cd52873da0b8c63275ebbe43f
Stuart Herbert <stuart@stuartherbert.com>
2014-06-28 10:07:18 +0100

    New: we need some CLI options supported by all switches

---
+++ src/php/DataSift/Storyplayer/Cli/Common/DeviceSupport.php
@@ -0,0 +1,133 @@
+<?php
+
+/**
+ * Copyright (c) 2011-present Mediasift Ltd
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the names of the copyright holders nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  Libraries
+ * @package   Storyplayer/Cli
+ * @author    Stuart Herbert <stuart.herbert@datasift.com>
+ * @copyright 2011-present Mediasift Ltd www.datasift.com
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link      http://datasift.github.io/storyplayer
+ */
+
+namespace DataSift\Storyplayer\Cli;
+
+use Exception;
+use stdClass;
+use Phix_Project\CliEngine;
+use Phix_Project\CliEngine\CliCommand;
+use Phix_Project\ExceptionsLib1\Legacy_ErrorHandler;
+use Phix_Project\ExceptionsLib1\Legacy_ErrorException;
+use DataSift\Stone\ConfigLib\E5xx_ConfigFileNotFound;
+use DataSift\Stone\ConfigLib\E5xx_InvalidConfigFile;
+use DataSift\Stone\LogLib\Log;
+use DataSift\Storyplayer\PlayerLib\E4xx_NoSuchReport;
+use DataSift\Storyplayer\PlayerLib\PhasesPlayer;
+use DataSift\Storyplayer\PlayerLib\StoryContext;
+use DataSift\Storyplayer\PlayerLib\StoryPlayer;
+use DataSift\Storyplayer\PlayerLib\StoryTeller;
+use DataSift\Storyplayer\PlayerLib\TalePlayer;
+use DataSift\Storyplayer\Console\DevModeConsole;
+
+/**
+ * Common support for selecting a device
+ *
+ * @category  Libraries
+ * @package   Storyplayer/Cli
+ * @author    Stuart Herbert <stuart.herbert@datasift.com>
+ * @copyright 2011-present Mediasift Ltd www.datasift.com
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link      http://datasift.github.io/storyplayer
+ */
+class Common_DeviceSupport implements Common_Functionality
+{
+    public function addSwitches(CliCommand $command, $additionalContext)
+    {
+        $command->addSwitches([
+            new DeviceSwitch($additionalContext->deviceList)
+        ]);
+    }
+
+    public function initFunctionality(CliEngine $engine, CliCommand $command, $injectables = null)
+    {
+        // shorthand
+        $output               = $injectables->output;
+        $staticConfig         = $injectables->staticConfig;
+        $staticConfigManager  = $injectables->staticConfigManager;
+
+        // do we need to load device-specific config?
+        //
+        // we do this AFTER loading environments, because that's the order
+        // we've told users it will happen in
+        if (isset($engine->options->device)) {
+            $deviceName = $engine->options->device;
+        }
+        else {
+            $deviceName = null;
+        }
+
+        if ($deviceName)
+        {
+            try {
+                // load our device-specific config
+                //
+                // this will be merged in with the default config
+                $staticConfigManager->loadAdditionalConfig($staticConfig, $deviceName);
+            }
+            catch (E5xx_ConfigFileNotFound $e) {
+                // do we already have this device?
+                if (!isset($staticConfig->devices->$deviceName)) {
+                    // no we don't ... report an error
+                    $msg = "no config file '{$deviceName}.json' found" . PHP_EOL;
+                    $output->logCliError($msg);
+                    exit(1);
+                }
+
+                // remember what our chosen device is
+                $staticConfig->initDevice($deviceName);
+            }
+            catch (E5xx_InvalidConfigFile $e) {
+                $msg = "unable to load config file '{$deviceName}.json'" . PHP_EOL . PHP_EOL
+                       . $e->getMessage();
+                $output->logCliError($msg);
+                exit(1);
+            }
+            catch (E4xx_NoSuchDevice $e) {
+                $msg = $e->getMessage();
+                $output->logCliError($msg);
+                exit(1);
+            }
+        }
+    }
+}
