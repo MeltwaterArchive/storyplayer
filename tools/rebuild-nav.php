c67469f10c42fe0de879ef3e6ff671f38d5c66aa
Stuart Herbert <stuart@stuartherbert.com>
2015-02-05 13:30:26 +0000

    Fix: switch to /relative URLs for sidebars

--- tools/rebuild-nav.php
+++ tools/rebuild-nav.php
@@ -162,16 +162,10 @@ function buildSidebar($sidebar, $toc, $pages)
 		$page = $pages["indexed"][$filename];
 
 		// build the href for the link
-		$parts = explode("/", $page['name']);
-		if (count($parts) > 1) {
-			$href = implode("/", array_slice($parts, 1));
-		}
-		else {
-			$href = $page['name'];
-		}
+		$href = BASE_URL . MANUAL_VERSION . '/' . $page['name'] . ".html";
 
 		// add the links into the sidebar
-		$sidebarHtml .= '<li><a href="' . $href . '.html">' . $page['title'] . "</a></li>\n";
+		$sidebarHtml .= '<li><a href="' . $href . '">' . $page['title'] . "</a></li>\n";
 		// echo $page['title'] . "\n";
 
 		if (isset($page['h2']))
@@ -179,7 +173,7 @@ function buildSidebar($sidebar, $toc, $pages)
 			$sidebarHtml .="<ul>\n";
 			foreach ($page['h2'] as $h2)
 			{
-				$sidebarHtml .= '<li><a href="' . $href . '.html#' . $h2['id'] . '">' . $h2['text'] . "</a></li>\n";
+				$sidebarHtml .= '<li><a href="' . $href . '#' . $h2['id'] . '">' . $h2['text'] . "</a></li>\n";
 				// echo "- " . $h2["text"] . "\n";
 			}
 
