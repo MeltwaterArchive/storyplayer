6b05ebcdef838e2a5c9e513a907923571ef3d10f
Stuart Herbert <stuart@stuartherbert.com>
2015-03-22 10:38:17 +0000

    Fix: dsbuild file now installs ZMQ test deps correctly

--- dsbuild/vagrant-centos6-ssl.sh
+++ dsbuild/vagrant-centos6-ssl.sh
@@ -19,14 +19,14 @@ git clone https://github.com/zeromq/zeromq4-x.git
 # install PHP 5.5 from Webtatic repo
 yum install -y php55w php55w-devel php55w-pear
 echo | pecl install zmq-1.1.2
-cp /vagrant/src/tests/zmq/centos6-zmq.ini /etc/php.d/zmq.ini
+cp /vagrant/src/tests/stories/zmq/centos6-zmq.ini /etc/php.d/zmq.ini
 
 # install Supervisor
 yum install -y supervisor
 
 # install our ZMQ echo server
-cp /vagrant/src/tests/zmq/zmq-echo-server.php /usr/local/bin/zmq-echo-server.php
-cp /vagrant/src/tests/zmq/zmq-echo-server.conf /etc/supervisor.d/zmq-echo-server.conf
+cp /vagrant/src/tests/stories/zmq/zmq-echo-server.php /usr/local/bin/zmq-echo-server.php
+cp /vagrant/src/tests/stories/zmq/zmq-echo-server.conf /etc/supervisor.d/zmq-echo-server.conf
 
 # now we're ready to start supervisor
 /etc/init.d/supervisord start
